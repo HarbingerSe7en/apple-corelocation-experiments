e8a55cb265cc46c11cbdcc56c7c1efcf88514956
zja <junan_zhang@maxnetsys.com.cn>
2026-04-29 23:44:43 +0800

    fix: zero-pad MAC address hex bytes in Decode()
    Each byte in the BSSID must be zero-padded to 2 hex digits.
    Previously %x formatting would omit leading zeros per byte,
    producing invalid BSSIDs like '70:65:82:e2:72:2' instead of
    '70:65:82:e2:72:02'. Now uses %02x per byte via binary.BigEndian.

--- lib/mac/mac.go
+++ lib/mac/mac.go
@@ -8,23 +8,9 @@ import (
 )
 
 func Decode(i int64) string {
-	macHex := fmt.Sprintf("%x", i)
-	if len(macHex) != 12 {
-		// Fill it up to 12 with 0s in front
-		for i := 0; i < 13-len(macHex); i++ {
-			macHex = "0" + macHex
-		}
-	}
-	// Insert : between every 2 hex values
-	mac := ""
-	for i := 0; i < len(macHex); i += 2 {
-		if i+2 < len(macHex) {
-			mac += macHex[i:i+2] + ":"
-		} else {
-			mac += macHex[i:]
-		}
-	}
-	return mac
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(i))
+	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", b[2], b[3], b[4], b[5], b[6], b[7])
 }
 
 func Encode(mac string) (int64, error) {
